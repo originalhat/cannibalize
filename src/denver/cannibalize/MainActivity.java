8cdc166e4385c1452ff053e98cb0c0b65705d483
dgbsco <devin.g.brown@gmail.com>
2012-11-18 12:11:52 -0700

    adding button listeners, #1

--- src/denver/cannibalize/MainActivity.java
+++ src/denver/cannibalize/MainActivity.java
@@ -1,6 +1,8 @@
 package denver.cannibalize;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.maps.MapActivity;
 
@@ -17,9 +19,48 @@ public class MainActivity extends MapActivity {
         // mapView = (MapView) findViewById(R.id.map_view);
         // mapView.setBuiltInZoomControls(true);
 
+		listenForButtonClicks();
+
 	}
 
-    @Override
+    /** listen for main menu button clicks **/
+    private void listenForButtonClicks() {
+
+        /** find drinks **/
+		final Button findDrinksButton = (Button) findViewById(R.id.findDrinks);
+		findDrinksButton.setOnClickListener(new View.OnClickListener() {
+	        public void onClick(View v) {
+	            // Perform action on click
+	        }
+	    });
+
+	    /** find food **/
+		final Button findFoodButton = (Button) findViewById(R.id.findFood);
+		findFoodButton.setOnClickListener(new View.OnClickListener() {
+	        public void onClick(View v) {
+	            // Perform action on click
+	        }
+	    });
+
+	    /** find greens **/
+		final Button findGreensButton = (Button) findViewById(R.id.findGreen);
+	    findGreensButton.setOnClickListener(new View.OnClickListener() {
+	        public void onClick(View v) {
+	            // Perform action on click
+	        }
+	    });
+
+	    /** find rides **/
+		final Button findRidesButton = (Button) findViewById(R.id.findRides);
+	    findRidesButton.setOnClickListener(new View.OnClickListener() {
+	        public void onClick(View v) {
+	            // Perform action on click
+	        }
+	    });
+
+	}
+
+	@Override
     protected boolean isRouteDisplayed() {
         return false;
     }
