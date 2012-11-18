36672430cb4bf698108a4d8a78b319a498cf3904
dgbsco <devin.g.brown@gmail.com>
2012-11-18 12:24:59 -0700

    adding TODO / program flow comments, trimming whitespace, general formatting, #1

--- src/denver/cannibalize/MainActivity.java
+++ src/denver/cannibalize/MainActivity.java
@@ -8,61 +8,71 @@ import com.google.android.maps.MapActivity;
 
 public class MainActivity extends MapActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		
-		// MapView mapView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        // MapView mapView;
         // mapView = (MapView) findViewById(R.id.map_view);
         // mapView.setBuiltInZoomControls(true);
 
-		listenForButtonClicks();
+        listenForButtonClicks();
 
-	}
+    }
+
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
 
     /** listen for main menu button clicks **/
     private void listenForButtonClicks() {
 
         /** find drinks **/
-		final Button findDrinksButton = (Button) findViewById(R.id.findDrinks);
-		findDrinksButton.setOnClickListener(new View.OnClickListener() {
-	        public void onClick(View v) {
-	            // Perform action on click
-	        }
-	    });
+        final Button findDrinksButton = (Button) findViewById(R.id.findDrinks);
+        findDrinksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // search for close bars
+            }
+        });
 
-	    /** find food **/
-		final Button findFoodButton = (Button) findViewById(R.id.findFood);
-		findFoodButton.setOnClickListener(new View.OnClickListener() {
-	        public void onClick(View v) {
-	            // Perform action on click
-	        }
-	    });
+        /** find food **/
+        final Button findFoodButton = (Button) findViewById(R.id.findFood);
+        findFoodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // search for close restaurants
+            }
+        });
 
-	    /** find greens **/
-		final Button findGreensButton = (Button) findViewById(R.id.findGreen);
-	    findGreensButton.setOnClickListener(new View.OnClickListener() {
-	        public void onClick(View v) {
-	            // Perform action on click
-	        }
-	    });
+        /** find greens **/
+        final Button findGreensButton = (Button) findViewById(R.id.findGreen);
+        findGreensButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // search for close marijuana dispensaries
+            }
+        });
 
-	    /** find rides **/
-		final Button findRidesButton = (Button) findViewById(R.id.findRides);
-	    findRidesButton.setOnClickListener(new View.OnClickListener() {
-	        public void onClick(View v) {
-	            // Perform action on click
-	        }
-	    });
+        /** find rides **/
+        final Button findRidesButton = (Button) findViewById(R.id.findRides);
+        findRidesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // use an Intent to open contacts
+            }
+        });
 
-	}
-
-	@Override
-    protected boolean isRouteDisplayed() {
-        return false;
     }
 
+    // TODO: retrieve current GPS location
+
+    // TODO: query google maps for a 'target', e.g. taco bell, bars, etc.
+    // return and place location in coordinate form
+
+    // TODO: call google maps with GPS and 'target location'
+    // use current loc. coords. and 'target location' coords to display path
 }
