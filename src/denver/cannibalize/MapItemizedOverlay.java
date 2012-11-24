7f68b9a58415876f0ca7909415e93c6bd9bdf442
Devin Brown <devin.g.brown@gmail.com>
2012-11-24 08:23:42 -0700

    adding itemized overlay for maps location position, #1

---
+++ src/denver/cannibalize/MapItemizedOverlay.java
@@ -0,0 +1,27 @@
+package denver.cannibalize;
+
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class MapItemizedOverlay extends ItemizedOverlay {
+
+    public MapItemizedOverlay(Drawable defaultMarker) {
+        super(defaultMarker);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected OverlayItem createItem(int i) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
